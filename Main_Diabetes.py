29985c1a7935704e893d405d0b13ce9ab0567a6a
alematt0504 <163540211+alematt0504@users.noreply.github.com>
2024-11-17 13:04:40 +0100

    Add files via upload

--- Main_Diabetes.py
+++ Main_Diabetes.py
@@ -17,7 +17,7 @@ torch.manual_seed(42)
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 # LOAD DEL DATASET E PREPROCESSING DEI DATI
-dataset_path = "diabetes2000.csv"
+dataset_path = "diabetes1000.csv"
 dataframe = pd.read_csv(dataset_path)
 dataframe.drop('Patient', axis=1, inplace=True)     # non considero la prima colonna con l'ID univoco del paziente
 
@@ -30,11 +30,10 @@ Y = np.array(dataframe['Diabetes'])       # variabile target
 dataframe.drop('Diabetes', axis=1, inplace=True)
 dataset = np.array(dataframe)
 
-OWL_dataset = np.load("OWL_dataset2000.npy")      # dataset OWL
+OWL_dataset = np.load("OWL_dataset1000.npy")      # dataset OWL
 
 # nuovo dataset arricchito
 X = np.hstack((dataset, OWL_dataset))
-#X = OWL_dataset
 
 model = Simple_NeuralNetwork(X.shape[1])
 loss_criterion = BCELoss()
@@ -52,12 +51,12 @@ X_test = torch.tensor(X_test, dtype=torch.float).to(device)
 Y_train = torch.tensor(Y_train, dtype=torch.float).to(device)
 Y_test = torch.tensor(Y_test, dtype=torch.float).to(device)
 
-loss_history = train(model, X_train, Y_train, optimizer, loss_criterion, 1500)
+loss_history = train(model, X_train, Y_train, optimizer, loss_criterion, 1100)
 
 # test
 test_loss, test_accuracy, test_precision, test_recall, test_f1_s, conf_mat = test(model, X_test, Y_test, loss_criterion)
 
 show_loss_history(loss_history)
-#show_confusion_matrix(conf_mat)
+show_confusion_matrix(conf_mat)
 show_metrics(test_accuracy, test_precision, test_recall, test_f1_s)
 
--- OWL_Dataset_Creator.py
+++ OWL_Dataset_Creator.py
@@ -1,5 +1,3 @@
-import logging
-
 import numpy as np
 import pandas as pd
 import owlready2 as owl
@@ -7,7 +5,7 @@ from owlready2 import sync_reasoner_pellet
 from rdflib import Graph
 import networkx as nx
 from node2vec import Node2Vec
-import owlready2
+
 
 # SCRIPT CHE CONSENTE DI SALVARE (OFFLINE) GLI EMBEDDINGS OTTENUTI DALL'ONTOLOGIA OWL
 
@@ -27,17 +25,32 @@ def data_2_owl(dataset, features_list, onto):
                     hasFeature = 'has' + feature    # attributo per l'individuo (ES. hasFeature = hasAge)
                     setattr(individual, hasFeature, [data_sample[i]])
 
+
+# funzione che effettua il reasoning per dedurre le regole nell'ontologia
+def onto_reasoning(onto):
+    owl.reasoning.JAVA_MEMORY = 8000  # attribuzione della memoria al reasoner, 8 GB
+    owl.set_log_level(9)
+    try:
+        with onto:
+            sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)
+            onto.save("reasoned_diabetes_ontology.rdf")
+    except:
+        print('inference Failed')
+
+
 # avendo l'ontologia popolata, restituisce un embedding di un individuo usando OWL2VEC,
 # l'individuo avrà il nome della prima feature
-def owl_2_embedding(model, individual_iri):
+def owl_2_embedding(model, base_iri):
+    # iri dell'individuo di cui fare l'embedding (ES. individual_iri=http://www.semanticweb.org/alessiomattiace/ontologies/2024/9/untitled-ontology-8#Patient_1)
+    individual_iri = base_iri + data_sample[0]
     return np.array(model.wv[individual_iri])
 
 
 # {EMBEDDING CREATION PROCESS} ----------------------------------------
 
 # -----[configurazione]
-dataset_path = "toy_diabetes.csv"  # SU: https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset
-ontology_path = "diabetes_ontology-2.rdf"  # SU: https://bioportal.bioontology.org/ontologies/CDMONTO/?p=classes&conceptid=http%3A%2F%2Fwww.semanticweb.org%2Fkhaled%2Fontologies%2F2024%2F7%2FCDMOnto%23Diabetes&lang=en
+dataset_path = "diabetes1000.csv"  # SU: https://www.kaggle.com/datasets/iammustafatz/diabetes-prediction-dataset
+ontology_path = "diabetes_ontology.rdf"  # SU: https://bioportal.bioontology.org/ontologies/CDMONTO/?p=classes&conceptid=http%3A%2F%2Fwww.semanticweb.org%2Fkhaled%2Fontologies%2F2024%2F7%2FCDMOnto%23Diabetes&lang=en
 embedding_size = 15
 
 # -----[Popolo l'ontologia]
@@ -54,30 +67,12 @@ populated_ontology_path = "populated_" + ontology_path
 onto.save(populated_ontology_path)
 
 # -----[eseguo il reasoning]
-
-owlready2.reasoning.JAVA_MEMORY = 8000    # attribuzione della memoria al reasoner, 8 GB
-owlready2.set_log_level(9)
-
-try:
-    with onto:
-
-        sync_reasoner_pellet(infer_property_values=True, infer_data_property_values=True)  # aggiunta delle inferenze all'ontologia
-        unsat = len(list(onto.inconsistent_classes()))     # controllo delle classi inconsistenti
-        if unsat > 0:
-            logging.warning("There are " + str(unsat) + " unsatisfiabiable classes.")
-        print("Ontology successfully inferred using Pellet.")
-
-except:
-    print('Failet to use pellet for inference')
-
-
+onto_reasoning(onto)
 
 # -----[ottengo gli embeddings]
-
-
 # grafo rdf dell'ontologia
 rdf_graph = Graph()
-rdf_graph.parse("prova_onto.rdf")
+rdf_graph.parse("reasoned_diabetes_ontology.rdf")
 
 # trasformazione del grafo rdf in grafo networkx per node2vec
 nx_graph = nx.Graph()
@@ -91,12 +86,9 @@ model = node2vec.fit()
 
 OWL_dataset = np.empty((0, embedding_size))
 for data_sample in dataset:
-
-    # iri dell'individuo di cui fare l'embedding
-    individual_iri = onto.base_iri + data_sample[0]  # (ES. individual_iri=http://www.semanticweb.org/alessiomattiace/ontologies/2024/9/untitled-ontology-8#Patient_1)
-
-    # calcolo l'embedding di ogni individuo dall'ontologia usando il suo iri
-    individual_embedding = owl_2_embedding(model, individual_iri)
+    base_iri = onto.base_iri
+    # calcolo l'embedding di ogni individuo dall'ontologia
+    individual_embedding = owl_2_embedding(model, base_iri)
     OWL_dataset = np.vstack((OWL_dataset, individual_embedding))
 
-np.save("OWL_dataset2000.npy", OWL_dataset)
+np.save("OWL_dataset1000.npy", OWL_dataset)
